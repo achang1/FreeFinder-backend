dc4d0f5076e4df29fa48afcee5445d3e218c837b
Angela Chang <a.chang@alumni.ubc.ca>
2018-04-13 21:14:01 -0700

    added post class bugged

--- ff_flask.py
+++ ff_flask.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api, reqparse, current_app
-from sqlalchemy import create_engine, select, insert, MetaData, Table, Column, Integer, String, ForeignKey, Sequence
+from sqlalchemy import create_engine, Float, DateTime, select, insert, MetaData, Table, Column, Integer, String, ForeignKey, Sequence
 import decimal, datetime
 import json
 import bcrypt
@@ -35,6 +35,20 @@ users = Table('users', metadata,
     Column('location', String(50)),
 )
 
+posts = Table('posts', metadata,
+    Column('id', Integer, Sequence('post_id_seq'), primary_key=True),
+    Column('user_id', Integer, ForeignKey("users.id"), nullable=False),
+    Column('title', String(50), nullable=False),
+    Column('time', DateTime, nullable=False),
+    Column('expiry', DateTime),
+    Column('last_modified', DateTime, onupdate=datetime.now, nullable=False),
+    Column('description', String(5000), nullable=False),
+    Column('location', String(50), nullable=False),
+    Column('lat', Float),
+    Column('lon', Float),
+)
+
+
 @jwt.jwt_data_loader
 def add_claims_to_access_token(email):
     now = datetime.utcnow()
@@ -54,6 +68,23 @@ def alchemyencoder(obj):
         return obj.isoformat()
     elif isinstance(obj, decimal.Decimal):
         return float(obj)
+
+class Post(Resource):
+    def get(self, post_id): #HTTP verb to retrieve something
+        try:
+            stmt = select([posts.c.id, posts.c.user_id, posts.c.title, posts.c.time, posts.c.expiry, posts.c.last_modified,
+                           posts.c.description, posts.c.location, posts.c.lat, posts.c.lon]) \
+                .select_from(posts).where(posts.c.id == post_id)
+            res = conn.execute(stmt)
+            res_dict = [dict(r) for r in res]
+            if len(res_dict) == 0:
+                return {}, 404
+            return_dict = json.dumps([dict(r) for r in res], default=alchemyencoder)
+            print(return_dict)
+            return return_dict
+
+        except Exception as e:
+            return {'error': str(e)}
 
 class User(Resource):
     def get(self, user_id):
@@ -98,7 +129,7 @@ class Users(Resource):
             return {'error': str(e)}
 
 
-    def post(self):
+    def post(self):     #create user
         # Parse the arguments
         parser = reqparse.RequestParser()
         self.make_parser_args(parser)
@@ -162,6 +193,7 @@ class Login(Resource):
 api.add_resource(Users, '/users')
 api.add_resource(User, '/users/<string:user_id>')
 api.add_resource(Login, '/login')
+api.add_resource(Post, '/posts/<string:post_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
