6356ee74630438ffaa3c9b1b87a83ef030e4c28a
Angela Chang <a.chang@alumni.ubc.ca>
2018-05-24 12:49:51 -0700

    updated PUT post

--- Models/posts.py
+++ Models/posts.py
@@ -1,10 +1,5 @@
 from flask_restful import Resource, reqparse
 from sqlalchemy import select, DateTime, update
-
-from flask_jwt_simple import (
-    jwt_required, create_jwt, get_jwt
-)
-
 import json
 from flask_jwt_simple import (
     jwt_required, create_jwt, get_jwt
@@ -12,6 +7,17 @@ from flask_jwt_simple import (
 from Models import data, helperFns
 
 class Post(Resource):
+    def make_parser_args(self, parser):
+        parser.add_argument('user_id', type=int, help='User of post')
+        parser.add_argument('title', type=str, help='Title of post')
+        parser.add_argument('time', type=str, help='Time of post')
+        parser.add_argument('expiry', type=str, help='Expiry time of post')
+        parser.add_argument('last_modified', type=str, help='Last modified time of post')
+        parser.add_argument('description', type=str, help='Description of post')
+        parser.add_argument('location', type=str, help='Location to create user')
+        parser.add_argument('lat', type=float, help='Latitude of post')
+        parser.add_argument('lon', type=float, help='Longitude time of post')
+
     def get(self, post_id): #HTTP verb to retrieve something
         try:
             stmt = select([data.posts.c.id, data.posts.c.user_id, data.posts.c.title, data.posts.c.time, data.posts.c.expiry, data.posts.c.last_modified,
@@ -28,43 +34,41 @@ class Post(Resource):
         except Exception as e:
             return {'error': str(e)}
 
-    # @jwt_required
-    # def put(self, post_id, user_id):
-    #     parser = reqparse.RequestParser()
-    #     self.make_parser_args(parser)
-    #     args = parser.parse_args()
-    #
-    #     _postTitle = args['title']
-    #     # _postTime = args['time']
-    #     # _postExpiry = args['expiry']
-    #     _postLastModified = args['last_modified']
-    #     _postDescription = args['description']
-    #     _postLocation = args['location']
-    #     _postLat = args['lat']
-    #     _postLon = args['lon']
-    #
-    #     #TODO: validate user (only valid user can modify)
-    #
-    #     return
-    #
-    #     try:
-    #         stmt = select(
-    #             [data.posts.c.id, data.posts.c.user_id, data.posts.c.title, data.posts.c.time, data.posts.c.expiry,
-    #              data.posts.c.last_modified, data.posts.c.description, data.posts.c.location, data.posts.c.lat, data.posts.c.lon]) \
-    #             .select_from(data.posts).where(data.posts.c.id == post_id)
-    #         res = data.conn.execute(stmt)
-    #         res_dict = [dict(r) for r in res]
-    #         if len(res_dict) == 0:
-    #             return {}, 404
-    #
-    #         stmt = update(data.posts).where(data.posts.c.id == user_jwt['id']). \
-    #             values(title=_postTitle, last_modified=_postLastModified, description=_postDescription,
-    #                    location=_postLocation, lat=_postLat, lon=_postLon)
-    #         res = data.conn.execute(stmt)
-    #         return {}, 200
-    #
-    #     except Exception as e:
-    #         return {'error': str(e)}
+    @jwt_required
+    def put(self, post_id, user_id):
+        parser = reqparse.RequestParser()
+        self.make_parser_args(parser)
+        args = parser.parse_args()
+
+        _postTitle = args['title']
+        _postLastModified = args['last_modified']
+        _postDescription = args['description']
+        _postLocation = args['location']
+        _postLat = args['lat']
+        _postLon = args['lon']
+
+        user_jwt = get_jwt()
+        jwt_user_id = user_jwt['id']
+        if jwt_user_id != int(user_id):
+            return {}, 403
+
+        try:
+            stmt = select([data.posts.c.id, data.posts.c.user_id, data.posts.c.title, data.posts.c.time, data.posts.c.expiry,
+                 data.posts.c.last_modified, data.posts.c.description, data.posts.c.location, data.posts.c.lat, data.posts.c.lon]) \
+                .select_from(data.posts).where(data.posts.c.id == post_id)
+            res = data.conn.execute(stmt)
+            res_dict = [dict(r) for r in res]
+            if len(res_dict) == 0:
+                return {}, 404
+
+            stmt = update(data.posts).where(data.posts.c.id == post_id). \
+                values(title=_postTitle, last_modified=_postLastModified, description=_postDescription,
+                       location=_postLocation, lat=_postLat, lon=_postLon)
+            res = data.conn.execute(stmt)
+            return {}, 200
+
+        except Exception as e:
+            return {'error': str(e)}
 
 class Posts(Resource):
     def make_parser_args(self, parser):
@@ -103,11 +107,9 @@ class Posts(Resource):
         jwt_user_id = user_jwt['id']
         # print("user jwt id:", jwt_user_id)
 
-        # _postUserId = args['user_id']
         _postTitle = args['title']
         _postTime = args['time']
         _postExpiry = args['expiry']
-        # _postLastModified = args['last_modified']
         _postDescription = args['description']
         _postLocation = args['location']
         _postLat = args['lat']
